31ef1547fd5bcb4f6024f12e3dbb7b44904f7e8b
Raf Gemmail <rafiq@dreamthought.com>
2019-04-14 12:57:11 +1200

    Added @Tags and @DisplayName

--- src/test/java/com/openclassrooms/testing/ConversionCalculatorTest.java
+++ src/test/java/com/openclassrooms/testing/ConversionCalculatorTest.java
@@ -2,6 +2,7 @@ package com.openclassrooms.testing;
 
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
@@ -11,36 +12,39 @@ import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.assertThat;
 
 @Tag("ConversionTests")
+@DisplayName("We should be able to convert between different units.")
 public class ConversionCalculatorTest {
-
     private ConversionCalculator calculatorUnderTest = new ConversionCalculator();
 
-
-    @Tag("FahrenheitTests")
+    @Nested
     @Tag("TemperatureTests")
-    @DisplayName("Given zero degrees in celsius When converted to Fahrenheit Then the result is 32")
-    @Test
-    public void celsiusToFahrenheit_returnsAFahrenheitTempurature_whenCelsiusIsPositive() {
-        Double actualFahrenheit = calculatorUnderTest.celsiusToFahrenheit(0.0 );
-        assertThat( actualFahrenheit, is(equalTo(32.0)) );
+    @DisplayName("We should be able to covert between Fahrenheit and Celsius")
+    class TemperatureTests {
+        @Tag("FahrenheitTests")
+        @DisplayName("Given zero degrees in celsius When converted to Fahrenheit then the result is 32")
+        @Test
+        public void celsiusToFahrenheit_returnsAFahrenheitTempurature_whenCelsiusIsPositive() {
+            Double actualFahrenheit = calculatorUnderTest.celsiusToFahrenheit(0.0 );
+            assertThat( actualFahrenheit, is(equalTo(32.0)) );
+        }
+
+        @DisplayName("Given 32 degrees in Fahrenheit when converted to celsius then the result is zero")
+        @Test
+        public void fahrenheitToCelsius() {
+            Double actualCelsius = calculatorUnderTest.fahrenheitToCelsius(32.0);
+            assertThat( actualCelsius, is(equalTo(0.0)));
+        }
     }
 
-
-    @Tag("TemperatureTests")
-    @DisplayName("Given 32 degrees in Fahrenheit when converted to celsius Then the result is zero")
     @Test
-    public void fahrenheitToCelsius() {
-        Double actualCelsius = calculatorUnderTest.fahrenheitToCelsius(32.0);
-        assertThat( actualCelsius, is(equalTo(0.0)));
-    }
-
-    @Test
+    @DisplayName("Given a volume in litres when converted to gallons then the result is rounded up")
     public void litresToGallons() {
         Double actualLitres = calculatorUnderTest.litresToGallons(3.78541);
         assertThat( actualLitres, is(equalTo(1.0)));
     }
 
     @Test
+    @DisplayName("Given a radius of one when converted then the result is Pi")
     public void radiusToAreaOfCircle() {
         Double actualArea = calculatorUnderTest.radiusToAreaOfCircle(1.0);
         assertThat( actualArea, is(equalTo(PI)));
